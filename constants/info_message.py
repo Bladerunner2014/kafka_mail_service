816b5b5557698f797bb46620208f2e512b0add6e
Mohammad Hossein Heidary <mohammadhsein9@gmail.com>
2023-02-03 16:56:20 +0330

    final docker

---
+++ constants/info_message.py
@@ -0,0 +1,13 @@
+class InfoMessage:
+    PRODUCING = "producer started to produce"
+    PRODUCING_FAIL = "producing failed"
+    LISTENING_FAIL = "consuming failed"
+    LISTENING = "consumer started listening"
+    LOGIN_REQUEST = "login request"
+    LOGIN_RESPONSE = "login response"
+
+    EMAIL_SENT = "email has been sent"
+
+    DB_QUERY = "db query"
+
+    KAFKA_PRODUCE = "kafka produce"
---
+++ manager/mail_app.py
@@ -0,0 +1,55 @@
+from smtplib import SMTP_SSL
+from email.message import EmailMessage
+from dotenv import dotenv_values
+from constants.error_message import ErrorMessage
+from constants.info_message import InfoMessage
+from constants.status_code import StatusCode
+import logging
+from http_handler.request_handler import RequestHandler
+from http_handler.response_handler import ResponseHandler
+
+logger = logging.getLogger(__name__)
+req = RequestHandler()
+config = dotenv_values(".env")
+res = ResponseHandler()
+
+
+def send_mail(contacts):
+    msg = EmailMessage()
+    msg["From"] = config["EMAIL_SENDER"]
+
+    msg["Subject"] = contacts["subject"]
+    try:
+        with SMTP_SSL("smtp.gmail.com", 465) as smtp:
+            for contact in contacts:
+                # to get html temp and format it with user details
+                with open(get_html_temp(contacts["template_id"]), 'r+') as level_1:
+                    string_html_temp = level_1.read().format(**contact)
+                msg.add_alternative(string_html_temp, subtype="html")
+                smtp.login(config["EMAIL_SENDER"], config["PASSWORD"])
+                smtp.send_message(msg, to_addrs=contact["email"])
+                logger.info(InfoMessage.EMAIL_SENT)
+
+    except Exception as error:
+        logger.error(ErrorMessage.HTML_FILE)
+        logger.error(error)
+        raise Exception
+    res.set_status_code(StatusCode.SUCCESS)
+    return res
+
+
+def get_html_temp(template_id):
+    request_json = req.create_json_from_args(template_id=template_id)
+    try:
+        result = req.send_post_request(base_url=config["MAIL_BASE_URL"],
+                                       end_point=config["MAIL_POST_URL"],
+                                       port=config["MAIL_PORT"],
+                                       body=request_json,
+                                       timeout=config["MAIL_TIMEOUT"],
+                                       error_log_dict={"message": ErrorMessage.BAD_REQUEST})
+    except Exception as error:
+        logger.error(ErrorMessage.HTML_DB)
+        logger.error(error)
+        raise Exception
+
+    return result
